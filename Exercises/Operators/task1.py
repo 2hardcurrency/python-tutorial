bb669f26a45e0aa777cbd500cbb992a2c8e3d40e
Igwegbe George Ifesinachi <118584274+2hardcurrency@users.noreply.github.com>
2023-03-15 19:29:23 -0700

    Update task1.py

--- Exercises/Operators/task1.py
+++ Exercises/Operators/task1.py
@@ -1,2 +1,10 @@
 # Write a Python program that calculates the sum and product of two numbers entered by the user.
 # Write your program below
+
+num = input("Enter two digit numbers: ").split()
+
+a = int(num[0])
+b = int(num[1])
+
+sum = a + b
+print("{} + {} = {}".format(a, b, sum(a, b)))
